547f3d1586293121428ba872106c74de48ccd019
Tom Smyth <tom@sassafras.coop>
2019-05-22 09:13:09 -0400

    Fix menu display bugs

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,9 +3,8 @@ function toggleMenu(toggle, event) {
   var showLink = document.querySelector('.menu-toggle-show');
   var hideLink = document.querySelector('.menu-toggle-hide');
   var menu = document.querySelector('.menu');
-  console.log(hideLink)
-  console.log(menu)
-  showLink.style.display = toggle ? 'none' : 'block';
-  hideLink.style.display = toggle ? 'block' : 'none';
-  menu.style.display = toggle ? 'block' : 'none';
+
+  showLink.classList[toggle ? 'add' : 'remove']('menu-hidden');
+  hideLink.classList[toggle ? 'remove' : 'add']('menu-hidden');
+  menu.classList[toggle ? 'remove' : 'add']('menu-hidden');
 }
